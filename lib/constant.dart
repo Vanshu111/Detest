31296155ab91d13daec136ec88d0370cfc393ad3
Vanshu111 <98654412+Vanshu111@users.noreply.github.com>
2023-03-20 11:28:01 +0530

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -73,7 +73,7 @@ class Device {
   });
 
   factory Device.fromJson(Map<String, dynamic> dvc, nowInMS) {
-    if (dvc['status'] == 'empty') {
+    if (dvc['status'] == 'inactive') {
       return Device(
         deviceId: dvc['device_id'],
         registerStatus: dvc['register_status'],
